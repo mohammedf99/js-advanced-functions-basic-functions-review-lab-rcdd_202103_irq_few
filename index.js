81be4e1c81a6dfb980617262fb01bd6d2835eee2
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-15 13:01:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,21 @@ const encouragingPromptFunction = wrapAdjective("!!!")
 
 const Calculator = {
   add(a, b) {
+    
     return a + b;
+    
   },
   subtract(a, b) {
     return a - b;
+  },
+  multiply(a, b) {
+    
+    return a * b;
+    
+  },
+  divide(a, b) {
+    
+    return a / b;
+    
   }
 }
