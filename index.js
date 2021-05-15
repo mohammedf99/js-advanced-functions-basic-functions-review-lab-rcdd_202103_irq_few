db0e336cf456de8010f0642bdeb099970d1d10db
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-15 13:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
 
-function mondayWork(activity = "got to the office") {
+function mondayWork(activity = "go to the office") {
   return `This Monday, I will ${activity}.`;
 }
 
